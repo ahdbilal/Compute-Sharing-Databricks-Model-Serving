17fda6e39f291a839f22966fecbcbe2555105735
ahdbilal <ahdbilal>
2023-06-18 22:23:54 +0000

    update structure

--- Sharing Endpoint Compute Across Multiple Models.py
+++ Sharing Endpoint Compute Across Multiple Models.py
@@ -7,7 +7,7 @@
 import os
 import pandas as pd
 import tempfile
-from utils import merge_requirements
+from packaging.version import parse as parse_version
 
 import mlflow
 from mlflow.models.signature import infer_signature
@@ -21,7 +21,35 @@ from sklearn.tree import DecisionTreeClassifier
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Train Two Models for Demonstrating Compute Sharing
+# MAGIC ## Function to Merge and Resolve Conflicting Requirements Across Models
+
+# COMMAND ----------
+
+# It is recommended to maintain consistent package versions across all logged models to prevent potential conflicts.
+# In cases where differences exist, the following function merges 'requirements.txt' files from each model, prioritizing the highest version number when a conflict arises.
+
+def merge_requirements(directory):
+    requirements = {}
+
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.startswith('requirements') and filename.endswith('.txt'):
+                with open(os.path.join(root, filename), 'r') as f:
+                    for line in f:
+                        line = line.strip()
+                        if '==' in line:
+                            name, version = line.split('==')
+                            if name not in requirements or parse_version(version) > parse_version(requirements.get(name, "0")):
+                                requirements[name] = version
+                        else:
+                            requirements[line] = requirements.get(line)
+
+    return [f"{name}=={version}" if version else name for name, version in requirements.items()]
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Training Two Models for Demonstrating Compute Sharing
 
 # COMMAND ----------
 
@@ -167,7 +195,3 @@ loaded_model = mlflow.pyfunc.load_model(model_uri)
 
 # Use the loaded model to make a prediction
 print(loaded_model.predict(input_example))
-
-# COMMAND ----------
-
-
